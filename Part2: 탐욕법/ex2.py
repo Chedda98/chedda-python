1bdeb70204eaf2dfc15e69c53015272557cc4f07
Chedda98 <twa05157@gmail.com>
2022-08-09 00:05:54 +0900

    Update: 1449. 수리공 항승 예제 코드

---
+++ Part2: 탐욕법/ex2.py
@@ -0,0 +1,15 @@
+N, L = map(int, input().split())
+coord = [False] * 1001 # 좌표계 인덱스 1000번까지 false 처리
+for i in range(int, input().split()):
+  coord[i] = True # 구멍 있을 때마다 true 표시
+
+ans = 0 # 필요한 테이프 개수
+x = 0
+while x < 1001:
+  if coord[x]:
+    ans += 1
+    x += 1
+  else:
+    x += 1
+
+print(ans)
