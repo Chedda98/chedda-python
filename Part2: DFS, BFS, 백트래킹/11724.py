943c9a367eb0796403b3ae157f68f4b3f10dbcb5
Chedda98 <twa05157@gmail.com>
2022-08-20 18:56:18 +0900

    Update: 1127. 연결 요소의 개수 풀이

--- Part2: DFS, BFS, 백트래킹/11724.py
+++ Part2: DFS, BFS, 백트래킹/11724.py
@@ -1,11 +1,11 @@
 import sys
 
-sys.setrecursionlimit(10 ** 6)
+sys.setrecursionlimit(10 ** 6) # 재귀 최대 깊이 설정(대부분의 파이썬 재귀 최대 깊이의 기본 설정이 1,000회 이기 때문에 런타임 에러가 발생)
 input = sys.stdin.readline
 N, M = map(int, input().split())
-adj = [[0] * N for _ in range(N)]
+adj = [[0] * N for _ in range(N)] # 인접행렬
 for _ in range(M):
-  a, b = map(lambda x: x -1, map(int, input().split()))
+  a, b = map(lambda x: x -1, map(int, input().split())) # 람다 이용하여 a와 b -1씩
   adj[a][b] = adj[b][a] = 1
 
 ans = 0
@@ -19,7 +19,7 @@ def dfs(now):
 
 for i in range(N):
   if not chk[i]:
-    ans += 1
+    ans += 1 # 비어있는 곳 만나면 연결 요소 갯수 더한 후 True로 변경
     chk[i] = True
     dfs(i)
 
